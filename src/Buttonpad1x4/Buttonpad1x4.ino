95f008c9efa36397ca1e49efdcc559e5da8c3743
Ian White <ian@impressiver.com>
2015-10-18 11:02:36 -0700

    Add faster shift out for esp, gamma table, and react to button presses

--- src/Buttonpad1x4/Buttonpad1x4.ino
+++ src/Buttonpad1x4/Buttonpad1x4.ino
@@ -38,68 +38,89 @@ Hardware connections:
     at the pin 1 mark (notch in the end of the chip). See the datasheet for a
     diagram.
 
-      SN74HC595       Buttonpad     ESP-07/12
-      ---------       ---------     ---------
-    * 1  (QB)         RED +
-    * 2  (QC)         GREEN +
-    * 3  (QD)         BLUE +
-    * 4  (QE)         LED-GND2 -
-    * 5  (QF)         LED-GND1 -
-    * 6  (QG)         LED-GND3 -
-    * 7  (QH)         LED-GND4 -
-      8  (GND)                      GND
-     ---
-      9  (QH*)
-      10 (SRCLR*)                   5V
-      11 (SRCLK)                    5
-      12 (RCLK)                     4
-      13 (OE*)                      GND
-      14 (SER)                      15
-    * 15 (QA)         SWITCH +
-      16 (VCC)                      5V
-
+    SN74HC595      SPI    Buttonpad     ESP-07/12
+    ---------      ---    ---------     ---------
+    * 1  (QB)             RED +
+    * 2  (QC)             GREEN +
+    * 3  (QD)             BLUE +
+    * 4  (QE)             LED-GND2 -
+    * 5  (QF)             LED-GND1 -
+    * 6  (QG)             LED-GND3 -
+    * 7  (QH)             LED-GND4 -
+      8  (GND)                          GND
+     ---    
+      9  (QH*)    
+      10 (SRCLR*)                       5V
+      11 (SRCLK)   SCLK                 14 (SCL)
+      12 (RCLK)    CS                   15
+      13 (OE*)                          GND
+      14 (SER)     MOSI                 13
+    * 15 (QA)             SWITCH +
+      16 (VCC)                          5V
+         (NC)      MISO                 12 (NC)
 */
 
 #include <ESP8266WiFi.h>
 
+// Gamma correction
+const uint8_t PROGMEM GAMMA[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+
 // Register
 // --------
 // register bit masks
-const byte SWITCH_MASK    = B10000000;
-const byte RGB_MASK       = B01110000;
-const byte COL_MASK       = B00001111;
+static const byte SWITCH_MASK    = B10000000;
+static const byte RGB_MASK       = B01110000;
+static const byte COL_MASK       = B00001111;
 
 // control pins
-const int OE_PIN          = 15;
-const int SRCLR_PIN       = 2;
-const int SER_PIN         = 4;
-const int RCLK_PIN        = 0;
-const int SRCLK_PIN       = 5;
+static const int OE_PIN          = 15;
+static const int SRCLR_PIN       = 2;
+static const int SER_PIN         = 4;
+static const int RCLK_PIN        = 0;
+static const int SRCLK_PIN       = 5;
 
 
 // Button
 // ------
-const int BTN_GND_PINS[4]  = {12, 13, 14, 16};  // col: 2, 1, 3, 4
+static const int BTN_GND_PINS[4]  = {12, 13, 14, 16};  // col order: 2, 1, 3, 4
 
 
 // RGB
 // ---
-const int NUM_COLS        = 4;
-const byte RGB_MAX        = 255;
+static const int NUM_COLS        = 4;
+static const byte RGB_MAX        = 255;
 
 // Settings
 // --------
-const int STEP_DELAY      = 32;       // microseconds (default: 24)
-const int CYCLE_DELAY     = 32;     // microseconds (default: 0)
+static const int STEP_DELAY      = 2;        // microseconds (default: 24)
+static const int CYCLE_DELAY     = 2;        // microseconds (default: 0)
+
+static const int BTN_DEBOUNCE    = 150;      // microseconds (default: 100)
 
 // Init
 // ----
-int column                = -1;
-unsigned long was[4]      = {0, 0, 0, 0};
-unsigned int rgbs[4][3]   = {{255, 0, 0},{0, 255, 0},{0, 0, 255},{150, 0, 200}};
-int btns[4]               = {LOW, LOW, LOW, LOW};
-
-unsigned int rgbsfx[4][3] = {{255, 0, 0},{0, 255, 0},{0, 0, 255},{150, 0, 200}};
+static int column                = -1;
+static unsigned long prev[4]     = {0, 0, 0, 0};
+static unsigned int rgbs[4][3]   = {{200, 0, 0},{0, 200, 0},{0, 0, 200},{40, 40, 40}};
+static unsigned int rgbsfx[4][3] = {{200, 0, 0},{0, 200, 0},{0, 0, 200},{40, 40, 40}};
+static unsigned long btns[4]     = {0, 0, 0, 0};
 
 // Run
 // ---
@@ -126,21 +147,45 @@ void setup()
   digitalWrite(RCLK_PIN, LOW);
 
   // reset register
-  bytelatch(0B10001111);
+  // bytelatch(0B10001111);
 }
 
 void loop()
 {
   column = (column + 1) % NUM_COLS;
   sense();
-  step(&column);
-  if (CYCLE_DELAY) delayMicroseconds(CYCLE_DELAY / NUM_COLS);
   react();
+  step();
+  if (CYCLE_DELAY) delayMicroseconds(CYCLE_DELAY / NUM_COLS);
 }
 
 
 // Functions
 // ---------
+
+/**
+ * @brief read inputs (buttons)
+ */
+void sense()
+{
+  if(!digitalRead(BTN_GND_PINS[column])) btns[column] = false;
+  else if (!btns[column]) btns[column] = micros();
+}
+
+/**
+ * @brief modify baseline rgb values based on input stimuli
+ * 
+ */
+void react()
+{
+  if(btns[column] && (micros() > (btns[column] + BTN_DEBOUNCE))) {
+    rgbsfx[column][0] = rgbsfx[column][1] = rgbsfx[column][2] = 255;
+  } else {
+    rgbsfx[column][0] = pgm_read_byte(&GAMMA[rgbs[column][0]]);
+    rgbsfx[column][1] = pgm_read_byte(&GAMMA[rgbs[column][1]]);
+    rgbsfx[column][2] = pgm_read_byte(&GAMMA[rgbs[column][2]]);
+  }
+}
 
 /**
  * @brief Set the state (high, low) of each rgb pin for the current step
@@ -150,20 +195,20 @@ void loop()
  * The SN74HC595 doesn't support PWM out (aka "intensity"), but it is plenty
  * fast enough to cycle a few RGB leds.
  */
-void step(int *col)
+void step()
 {
-  byte data = 1U | ~(1U << (*col + 4U));
+  byte data = 1U | ~(1U << (column + 4U));
   bool on;
   
   // mark start of column cycle
   unsigned long now = micros();
 
   // turn on if:
-  //  workload greater than 0
-  //  256µs clock, modulo workload, is less than or equal to half the workload,
-  //    plus the between column cycles (to compensate for time the led was off)
+  //  workload is 255, or greater than 0 and:
+  //  the 255µs clock, modulo workload, is less than or equal to half the workload
+  //    plus the time between the prev and current cycle (to compensate for led being off)
   for(unsigned int i = 0; i < 3; i++) {
-    on = (rgbsfx[*col][i] > 0) && ((now % 256) % (rgbsfx[*col][i] + 1) <= (((rgbsfx[*col][i] + 1) / 2) + (now - was[*col])));
+    on = (rgbsfx[column][i] == 255) || (rgbsfx[column][i] && (((now - (now - prev[column])) % 255) * ((double)rgbsfx[column][i] / 255.0)) > ((double)rgbsfx[column][i] / 2.0));
     if (on) {
       data |= 1U << (i + 1U);
     } else {
@@ -171,25 +216,22 @@ void step(int *col)
     }
   }
 
-  bytelatch(data);
-  delayMicroseconds(STEP_DELAY);
+  // set output data
+  bytelatch(&data);
+  if(STEP_DELAY) delayMicroseconds(STEP_DELAY);
   // record end to calculate time between column cycles
-  was[*col] = micros();
+  prev[column] = micros();
 }
 
 /**
- * @brief Shift byte data into register and toggle data clock (latch)
+ * @brief Shift byte data into register and toggle latch
  * 
  * @param data byte to shift in (least significant bit first)
  */
-void bytelatch(byte data)
+void bytelatch(byte *data)
 {
-  // make sure clock is low
-  digitalWrite(SRCLK_PIN, LOW);
-  // shift data into position
-  shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, data);
-  // output in parallel
-  blip(RCLK_PIN, true, 0);
+  espShiftOut(*data);
+  blip(RCLK_PIN, HIGH, 0);
 }
 
 /**
@@ -205,31 +247,23 @@ void bytelatch(byte data)
  */
 void blip(unsigned int pin, bool on, unsigned long us)
 {
-  digitalWrite(pin, on ? HIGH : LOW);
+  digitalWrite(pin, on);
   if (us > 0) delayMicroseconds(us);
-  digitalWrite(pin, on ? LOW : HIGH);
+  digitalWrite(pin, !on);
 }
 
-void sense()
+void espShiftOut(byte data)
 {
-  btns[column] = digitalRead(BTN_GND_PINS[column]);
-}
-
-void react()
-{
-  int btn = digitalRead(BTN_GND_PINS[column]);
-  if (btn && !btns[column]) {
-    rgbsfx[column][0] = 255;
-    rgbsfx[column][1] = 0;
-    rgbsfx[column][2] = 0;
-  } else if(btn) {
-    rgbsfx[column][0] = 255;
-    rgbsfx[column][1] = 255;
-    rgbsfx[column][2] = 255;
-  } else {
-    rgbsfx[column][0] = rgbs[column][0];
-    rgbsfx[column][1] = rgbs[column][1];
-    rgbsfx[column][2] = rgbs[column][2];
-  }
-  btns[column] = btn;
+  byte i = 8;
+  do{
+    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << SRCLK_PIN);
+    
+    if(data & 0x80) GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << SER_PIN);
+    else GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << SER_PIN);
+    
+    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << SRCLK_PIN);
+    
+    data <<= 1;
+  } while(--i);
+  return;
 }
